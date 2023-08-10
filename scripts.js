eda836d7d10ed7fdf9d7ae379e08c970c13b4dcf
DecadeY <105374546+DecadeY@users.noreply.github.com>
2023-08-10 16:33:35 +1000

    fix

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
-
-
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
-            '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.subTitle + '</a></p></div>' +
-            '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.description + '</p>' +
-            '</div></div></div>';
+        let itemToAppend = '<div class="col s4 center-align">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
+                '</div><div class="card-content">'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+                '<div class="card-reveal">'+
+                '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+                '<p class="card-text">'+item.description+'</p>'+
+                '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
@@ -17,31 +15,31 @@ const addCards = (items) => {
 const formSumitted = () => {
     let formData = {};
     formData.title = $('#title').val();
-    formData.subTitle = $('#subTitle').val();
     formData.path = $('#path').val();
+    formData.subTitle = $('#subTitle').val();
     formData.description = $('#description').val();
 
     console.log(formData);
     postCat(formData);
 }
 
-function postCat(cat){
+function postCat(cat) {
     $.ajax({
-        url: '/api/cat',
+        url:'/api/cat',
         type:'POST',
-        date:cat,
-        success:(result)=>{
-            if (result.statusCode === 201){
-                alert('cat post successful');
+        data:cat,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('cat posted');
             }
         }
-    })
+    });
 }
 
-function getAllCats(){
-    $.get('/api/cats', (response)=>{
-        if(response.statusCode === 200){
-            addCards(response.date);
+function getAllCats() {
+    $.get('/api/cats',(result)=>{
+        if (result.statusCode === 200) {
+            addCards(result.data);
         }
     });
 }
@@ -52,5 +50,5 @@ $(document).ready(function(){
         formSumitted();
     });
     $('.modal').modal();
-    getAllCats
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -7,9 +7,19 @@ let port = process.env.PORT || 3000;
 let collection;
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+async function run() {
+    try {
+      await client.connect();
+      collection = client.db().collection('Cats');
+      console.log(collection);
+    } catch(ex) {
+      console.error(ex);
+    }
+}
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -17,51 +27,40 @@ const client = new MongoClient(uri, {
       strict: true,
       deprecationErrors: true,
     }
-  });
+});
 
-
-  async function runDBConnection() {
-    try {
-      await client.connect();
-      collection = client.db().collection('cat');
-      console.log(collection);
-    } catch(ex){
-        console.error(ex);
-    }
-  }
-  runDBConnection().catch(console.dir);
-
-app.get('/', (req, res) => {
+app.get('/', function (req,res) {
     res.render('index.html');
 });
 
-app.get('/api/cat',(req,res)=>{
-    getAllCats((err,result)=>{
-        if(!err){
-            res.json({statusCode:200, data:result, message:'get all cats successful'});
-        }
-    });
-});
-
-app.post('/api/cat',(req,res)=>{
+app.post('/api/cat', function(req,res){
+    // ask DB to post this CAT
     let cat = req.body;
-    postCat(cat,(err, result) => {
-        if(!err){
-            res.json({statusCode:201, data:result, message:'success'});
+    insertCat(cat, (err,result) => {
+        if (!err) {
+            res.json({statusCode:201,data:result,message:'success'});
         }
     });
 });
 
-function postCat(cat,callback){
+function insertCat(cat, callback) {
     collection.insertOne(cat,callback);
 }
 
-function getAllCats(callback){
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+});
+
+function getAllCats(callback) {
     collection.find({}).toArray(callback);
 }
 
-app.listen(port, () => {
-    console.log('server started');
-    runDBConnection();
+app.listen(port, ()=>{
+    console.log('express server started');
+    run();
 });
 
